8c4d8578296a4ae7efbef29ea9b215aa00c4daea
MerenovaAnastasiya <31702117+MerenovaAnastasiya@users.noreply.github.com>
2019-12-14 12:21:34 +0300

    Update 6.c

--- OS_lab5/6.c
+++ OS_lab5/6.c
@@ -2,14 +2,30 @@
 #include <dirent.h> 
 
 int main(int argc, char* argv[]) {
-
-	DIR *dr = opendir(argv[1]); 
-    if (dr == NULL)
+    DIR *current_dir = opendir("./"); 
+    if (current_dir == NULL)
     { 
         printf("Could not open current directory" ); 
         return 0; 
     }
-   	struct dirent *de; 
+    struct dirent *de; 
+    printf("files in current directory:\n");
+    while ((de = readdir(current_dir)) != NULL) 
+            printf("%s\n", de->d_name); 
+  
+    closedir(current_dir); 
+
+
+
+    DIR *dr = opendir(argv[1]); 
+    if (dr == NULL)
+    { 
+        printf("Could not open desired directory" ); 
+        return 0; 
+    }
+
+    printf("files in current directory:\n");
+
     while ((de = readdir(dr)) != NULL) 
             printf("%s\n", de->d_name); 
   
